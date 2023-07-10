17ab4a9e4f9af18d30eb7ad27bb2b946649140de
DDang <k3h1i013@gmail.com>
2023-07-11 01:09:53 +0900

    feat: Added Component-TBTag

---
+++ ios-tripbook/Component/TBTag.swift
@@ -0,0 +1,66 @@
+//
+//  TBTag.swift
+//  ios-tripbook
+//
+//  Created by DDang on 7/11/23.
+//
+
+import SwiftUI
+
+struct TBTag: View {
+    enum ViewType: String {
+        case awaitingApproval = "승인대기중"
+        case approved = "승인완료"
+        case rejected = "반려"
+    }
+    
+    let type: ViewType
+    let completion: (() -> Void)?
+    
+    init(_ type: ViewType, completion: (() -> Void)? = nil) {
+        self.type = type
+        self.completion = completion
+    }
+    
+    var body: some View {
+        Button(action: {
+            self.completion?()
+        }) {
+            HStack(spacing: 0) {
+                Text(self.type.rawValue)
+                    .font(TBFont.caption_2)
+                    .foregroundColor(
+                        self.type == .awaitingApproval ? TBColor.point.green[2] :
+                            self.type == .approved ? .white :
+                            self.type == .rejected ? TBColor.state.warning : .clear
+                    )
+                
+                if self.type == .rejected {
+                    TBIcon.next.iconSize(size: .tiny)
+                        .foregroundColor(TBColor.state.warning)
+                }
+            }
+            .padding(.vertical, 2)
+            .padding(.leading, 4)
+            .padding(.trailing, self.type != .rejected ? 4 : 0)
+            .background(
+                RoundedRectangle(cornerRadius: 4)
+                    .foregroundColor(
+                        self.type == .awaitingApproval ? TBColor.state.success :
+                            self.type == .approved ? TBColor.primary._50 :
+                            self.type == .rejected ? TBColor.grayscale._10 : .clear
+                    )
+            )
+        }
+    }
+}
+
+struct TBTag_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack(alignment: .leading) {
+            TBTag(.awaitingApproval)
+            TBTag(.approved)
+            TBTag(.rejected)
+        }
+    }
+}

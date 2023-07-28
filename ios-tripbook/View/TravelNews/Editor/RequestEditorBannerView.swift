8aaa0407e870640aee199b5dd39c06d9177c67df
Safari <111dltldnjs@gmail.com>
2023-07-28 18:12:53 +0900

    added: RequestEditorBannerView.swift 파일 생성

---
+++ ios-tripbook/View/TravelNews/Editor/RequestEditorBannerView.swift
@@ -0,0 +1,20 @@
+//
+//  RequestEditorBannerView.swift
+//  ios-tripbook
+//
+//  Created by 이시원 on 2023/07/28.
+//
+
+import SwiftUI
+
+struct RequestEditorBannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RequestEditorBannerView_Previews: PreviewProvider {
+    static var previews: some View {
+        RequestEditorBannerView()
+    }
+}

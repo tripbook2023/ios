2bb5328c8a80bc87ad26d68f31d993b0d4bef975
Safari <111dltldnjs@gmail.com>
2023-08-01 19:38:30 +0900

    added: RequestEditorGuideView.swift 파일 생성

---
+++ ios-tripbook/View/TravelNews/Editor/RequestEditorGuideView.swift
@@ -0,0 +1,20 @@
+//
+//  RequestEditorGuideView.swift
+//  ios-tripbook
+//
+//  Created by 이시원 on 2023/08/01.
+//
+
+import SwiftUI
+
+struct RequestEditorGuideView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RequestEditorGuideView_Previews: PreviewProvider {
+    static var previews: some View {
+        RequestEditorGuideView()
+    }
+}

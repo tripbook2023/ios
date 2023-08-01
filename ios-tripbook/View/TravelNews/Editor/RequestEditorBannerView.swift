781b257b4a9b303e07c52d6885490b0b577b581e
Safari <111dltldnjs@gmail.com>
2023-08-01 19:23:28 +0900

    feat: RequestEditorBannerView 구현

--- ios-tripbook/View/TravelNews/Editor/RequestEditorBannerView.swift
+++ ios-tripbook/View/TravelNews/Editor/RequestEditorBannerView.swift
@@ -9,7 +9,27 @@ import SwiftUI
 
 struct RequestEditorBannerView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("모두에게 내 여행을 자랑하고 싶다면,")
+                .font(TBFont.body_3)
+                .foregroundColor(TBColor.primary.levels[0])
+            Spacer().frame(height: 4)
+            HStack {
+                Text("여행 에디터")
+                    .foregroundColor(TBColor.primary.levels[5])
+                + Text("를 신청해보세요!")
+                    .foregroundColor(.white)
+            }.font(TBFont.heading_1)
+            
+            Spacer().frame(height: 11)
+            Image("EditorBanner")
+                .resizable()
+                .frame(width: 248, height: 200)
+            Spacer().frame(height: 8)
+            Text("에디터 신청만 해도 5,000P 지급!")
+                .font(TBFont.body_3)
+                .foregroundColor(TBColor.primary.levels[1])
+        }
     }
 }
 

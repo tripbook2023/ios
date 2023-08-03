b461a53f6a06f63c8d523c928ab5b3fee1d28486
Safari <111dltldnjs@gmail.com>
2023-08-03 18:00:01 +0900

    added: EditorBenefitsView.swift 파일 생성

---
+++ ios-tripbook/View/TravelNews/Editor/EditorBenefitsView.swift
@@ -0,0 +1,20 @@
+//
+//  EditorBenefitsView.swift
+//  ios-tripbook
+//
+//  Created by 이시원 on 2023/08/03.
+//
+
+import SwiftUI
+
+struct EditorBenefitsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditorBenefitsView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditorBenefitsView()
+    }
+}

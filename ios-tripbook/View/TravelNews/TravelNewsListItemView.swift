0a686440cc6b35c64908c71b8de42ca3d0144cfe
Safari <111dltldnjs@gmail.com>
2023-09-13 15:54:25 +0900

    added: TravelNewsListItemView 파일 생성

---
+++ ios-tripbook/View/TravelNews/TravelNewsListItemView.swift
@@ -0,0 +1,20 @@
+//
+//  TravelNewsListItemView.swift
+//  ios-tripbook
+//
+//  Created by 이시원 on 2023/09/13.
+//
+
+import SwiftUI
+
+struct TravelNewsListItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TravelNewsListItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        TravelNewsListItemView()
+    }
+}

5b40a763089eb3f6eb4c4e32dc530e38305d5308
Safari <111dltldnjs@gmail.com>
2023-09-19 21:35:15 +0900

    fixed: TBAPIManager APIManagerable 채택하여 구현

--- ios-tripbook/Network/Base/TBAPIManager.swift
+++ ios-tripbook/Network/Base/TBAPIManager.swift
@@ -8,16 +8,96 @@
 import Foundation
 import Alamofire
 
-struct TBAPIManager {
-    static let shared = TBAPIManager()
+struct TBAPIManager: APIManagerable {
+    private let session: Session
     
-    let sessionManager: Session = {
-        let configuration = URLSessionConfiguration.af.default
-        let interceptor = TBAuthInterceptor()
-        let apiLogger = TBAPIEventLogger()
+    init(session: Session = .default) {
+        self.session = session
+    }
+    
+}
+
+// MARK: - Request
+
+extension TBAPIManager {
+    func request<T: Decodable>(_ api: APIable, type: T.Type) async throws -> T {
+        let data = try await request(api)
+        let result = try JSONDecoder().decode(type, from: data)
         
-        configuration.timeoutIntervalForRequest = 30
+        return result
+    }
+    
+    func request(_ api: APIable) async throws -> Data {
+        let dataRequest = try dataRequest(api)
+        let result = dataRequest
+            .serializingResponse(using: .data)
         
-        return Session(configuration: configuration, interceptor: interceptor, eventMonitors: [apiLogger])
-    }()
+        return try await result.value
+    }
+    
+    private func dataRequest(_ api: APIable) throws -> DataRequest {
+        guard let url = URL(string: "\(api.baseURL)/\(api.path)") else {
+            fatalError("")
+        }
+        
+        return session.request(
+            url,
+            method: api.method,
+            parameters: api.parameters,
+            encoding: JSONEncoding.default,
+            headers: api.headers
+        ).validate(statusCode: 200..<300)
+    }
+}
+
+// MARK: - Upload
+
+extension TBAPIManager {
+    func upload<T: Decodable>(
+        _ api: APIable,
+        uploadImages: [Data],
+        withName: ImageUploadName,
+        type: T.Type
+    ) async throws -> T {
+        let data = try await upload(api, uploadImages: uploadImages, withName: withName)
+        let result = try JSONDecoder().decode(type, from: data)
+        
+        return result
+    }
+    
+    func upload(
+        _ api: APIable,
+        uploadImages: [Data],
+        withName: ImageUploadName
+    ) async throws -> Data {
+        let dataRequest = try uploadRequest(api, uploadImages: uploadImages, withName: withName)
+        let result = dataRequest
+            .serializingResponse(using: .data)
+        
+        return try await result.value
+    }
+    
+    private func uploadRequest(
+        _ api: APIable,
+        uploadImages: [Data],
+        withName: ImageUploadName
+    ) throws -> UploadRequest {
+        guard let url = URL(string: "\(api.baseURL)/\(api.path)") else {
+            fatalError("")
+        }
+        
+        return session.upload(
+            multipartFormData: { multipartFormData in
+                for (key, value) in api.parameters {
+                    multipartFormData.append("\(value)".data(using: .utf8)!, withName: key)
+                }
+                uploadImages.forEach { image in
+                    multipartFormData.append(image, withName: withName.rawValue, fileName: "\(image).jpeg", mimeType: "image/jpeg")
+                }
+            },
+            to: url,
+            usingThreshold: .init(),
+            headers: api.headers
+        ).validate(statusCode: 200..<300)
+    }
 }

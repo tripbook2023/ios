3de9f4f7b6ed97087501bf562b4727e285176617
Safari <111dltldnjs@gmail.com>
2023-10-05 14:12:43 +0900

    added: TravelNewsSortPopupView 파일 생성

---
+++ ios-tripbook/View/TravelNews/TravelNewsSortPopupView.swift
@@ -0,0 +1,20 @@
+//
+//  TravelNewsSortPopupView.swift
+//  ios-tripbook
+//
+//  Created by 이시원 on 2023/10/05.
+//
+
+import SwiftUI
+
+struct TravelNewsSortPopupView: View {
+    var body: some View {
+        
+    }
+}
+
+struct TravelNewsSortPopupView_Previews: PreviewProvider {
+    static var previews: some View {
+        TravelNewsSortPopupView()
+    }
+}

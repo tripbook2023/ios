4a59743ba450bdb4b8ad778a878a2b14f04e4f79
Safari <111dltldnjs@gmail.com>
2023-10-15 20:17:08 +0900

    feat: TravelNewsSearchLocationViewModel 타입 구현

--- ios-tripbook/ViewModel/TravelNews/TravelNewsSearchLocationViewModel.swift
+++ ios-tripbook/ViewModel/TravelNews/TravelNewsSearchLocationViewModel.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import Combine
+
+@MainActor
+class TravelNewsSearchLocationViewModel: ObservableObject {
+    private let apiManager: APIManagerable
+    private var task: Task<Void, Error>? = nil
+    private var anyCancellable = Set<AnyCancellable>()
+    
+    @Published var searchKeyword: String = ""
+    @Published var locationInfos: [LocationInfo] = []
+    
+    init(apiManager: APIManagerable = TBAPIManager()) {
+        self.apiManager = apiManager
+        $searchKeyword.sink { [weak self] keyword in
+            self?.task?.cancel()
+            self?.task = Task {
+                try await Task.sleep(nanoseconds: 1_000_000_000)
+                await self?.searchLocation(keyword: keyword)
+            }
+        }.store(in: &anyCancellable)
+    }
+    
+    private func searchLocation(keyword: String) async {
+        let result = try? await apiManager.request(
+            KakaoLocationSearchAPI.locationSearch(
+                query: keyword
+            ),
+            type: LocationSearchResponse.self
+        ).toDomain
+        
+        locationInfos = result ?? []
+    }
+}

41f83de5d48dea29001f591d2decd5a2c34ace28
Safari <111dltldnjs@gmail.com>
2023-10-26 14:31:33 +0900

    feat: OnboardingViewModel 구현

---
+++ ios-tripbook/ViewModel/OnboardingViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  OnboardingViewModel.swift
+//  ios-tripbook
+//
+//  Created by 이시원 on 2023/10/24.
+//
+
+import Foundation
+@MainActor
+class OnboardingViewModel: ObservableObject {
+    enum ViewState {
+        case root
+        case login
+    }
+    
+    @Published var presentView: ViewState? = nil
+    @Published var isHeddin = false
+    
+    private let apiManager: APIManagerable
+    private let tokenStorage: TokenStorage
+    
+    init(
+        apiManager: APIManagerable = TBAPIManager(),
+        tokenStorage: TokenStorage = .shared
+    ) {
+        self.apiManager = apiManager
+        self.tokenStorage = tokenStorage
+        requestTokenReissue(refreshToken: tokenStorage.refreshToken ?? "")
+    }
+    
+    private func requestTokenReissue(refreshToken: String) {
+        Task {
+            do {
+                let result = try await apiManager.request(TBMemberAPI.tokenReissue(refreshToken: refreshToken), type: TokenReissueResponse.self)
+                tokenStorage.setTokens(accessToken: result.accessToken, refreshToken: result.refreshToken)
+                presentView = .root
+            } catch {
+                presentView = .login
+            }
+            
+        }
+    }
+}

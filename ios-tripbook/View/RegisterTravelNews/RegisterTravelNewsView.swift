b2f427dc593dec4c83174b8eeff22921b5ef5259
Red <seryprk@gmail.com>
2023-11-03 18:48:02 +0900

    [feat] make header view

--- ios-tripbook/View/RegisterTravelNews/RegisterTravelNewsView.swift
+++ ios-tripbook/View/RegisterTravelNews/RegisterTravelNewsView.swift
@@ -18,24 +18,71 @@ struct RegisterTravelNewsView : UIViewControllerRepresentable {
     }
     
     func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
-        //
     }
 }
 
 class RegisterTravelReportVC: UIViewController {
+    
+    private var backButton: UIButton!
+    private var registerButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let textView = UITextView()
-        textView.text = "aa"
-        view.addSubview(textView)
-        
-        textView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
-    }
+        makeHeaderView()
 
-    @objc func buttonAction(sender: UIButton!) {
-        print("버튼이 클릭되었습니다.")
+    }
+    
+    private func makeHeaderView() {
+        let headerView = UIView()
+        
+        backButton = UIButton()
+        backButton.setImage(UIImage(named: "Before/01"), for: .normal)
+        backButton.tintColor = .black
+        
+        let headerTitleLabel = UILabel()
+        headerTitleLabel.text = "여행 소식 등록"
+        headerTitleLabel.font = .init(name: "SUIT-Medium", size: 16)
+        
+        headerView.addSubview(backButton)
+        backButton.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(20)
+            make.width.height.equalTo(24)
+            make.centerY.equalToSuperview()
+        }
+        
+        registerButton = UIButton()
+        
+        let customFont = UIFont(name: "SUIT-Medium", size: 16)
+
+        let attributes: [NSAttributedString.Key : Any] = [
+            .font: customFont!,
+            .foregroundColor: UIColor(red: 0.3, green: 0.27, blue: 0.26, alpha: 1)
+        ]
+
+        let attributedTitle = NSAttributedString(string: "등록", attributes: attributes)
+
+        registerButton.setAttributedTitle(attributedTitle, for: .normal)
+        
+        registerButton.backgroundColor = UIColor(red: 0.88, green: 0.86, blue: 0.86, alpha: 1)
+        registerButton.layer.cornerRadius = 6
+        
+        headerView.addSubview(headerTitleLabel)
+        headerTitleLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        
+        headerView.addSubview(registerButton)
+        registerButton.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().offset(-20)
+            make.height.equalTo(32)
+            make.width.equalTo(48)
+            make.centerY.equalToSuperview()
+        }
+        
+        view.addSubview(headerView)
+        headerView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.height.equalTo(48)
+        }
     }
 }

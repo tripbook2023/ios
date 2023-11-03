2bcac6185cd7ef7577fa21cee5a2d07b2fd644ca
Safari <111dltldnjs@gmail.com>
2023-11-03 19:56:28 +0900

    feat: ProfileView 구현

--- ios-tripbook/View/Profile/ProfileView.swift
+++ ios-tripbook/View/Profile/ProfileView.swift
@@ -14,8 +14,99 @@ import SwiftUI
  -
  */
 struct ProfileView: View {
+    var deviceWidth: CGFloat {
+        return UIScreen.main.bounds.width
+    }
+
     var body: some View {
-        Text("Profile")
+        VStack(spacing: 0) {
+            TBAppBar(
+                title: "마이페이지",
+                rightItem:  {
+                    Button {
+                        
+                    } label: {
+                        TBIcon.bell.iconSize(size: .medium)
+                    }
+                    .foregroundColor(.black)
+                }
+            )
+            .padding(.horizontal, 20)
+            HStack {
+                Image("DefaultProfileImage")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 80, height: 80)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle()
+                            .stroke(TBColor.grayscale._10, lineWidth: 0.57)
+                    )
+                
+                VStack(alignment: .leading) {
+                    Text("홍길동")
+                        .foregroundColor(.white)
+                        .font(TBFont.title_2)
+                    Text("sss@ssss.com")
+                        .tint(.white)
+                        .font(TBFont.caption_1)
+                }
+                Spacer()
+                Button {
+                    // 프로필 설정 로직
+                } label: {
+                    TBIcon.setting.iconSize(size: .medium)
+                        .foregroundColor(.white)
+                }
+
+            }
+            .padding(.horizontal, 20)
+            .frame(width: deviceWidth, height: 128)
+            .background(TBColor.primary._50)
+            
+            Button {
+                //1:1 문의 로직
+            } label: {
+                HStack {
+                    Text("1:1 문의")
+                        .foregroundColor(TBColor.grayscale._90)
+                    Spacer()
+                    TBIcon.next.iconSize(size: .medium)
+                        .foregroundColor(TBColor.grayscale._50)
+                }
+                .padding(.vertical, 16)
+                .frame(width: deviceWidth - 40)
+            }
+            Divider()
+                .background(TBColor.grayscale._5)
+            VStack {
+                Button {
+                    //로그아웃 로직
+                    
+                } label: {
+                    RoundedRectangle(cornerRadius: 6)
+                        .strokeBorder(TBColor.grayscale._50, lineWidth: 1)
+                        .foregroundColor(.white)
+                        .overlay(
+                            Text("로그아웃")
+                                .font(TBFont.body_4)
+                        )
+                        
+                        
+                }
+                .frame(width: 335, height: 32)
+                .padding(.top, 24)
+                .padding(.bottom, 12)
+
+                Text("버전정보 v1.0.0")
+                    .font(TBFont.caption_1)
+                    .foregroundColor(TBColor.grayscale._40)
+                Spacer()
+            }
+            .frame(width: deviceWidth)
+            .background(TBColor.grayscale._1)
+        }
+        
     }
 }
 

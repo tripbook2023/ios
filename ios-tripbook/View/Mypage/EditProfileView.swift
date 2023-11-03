e6abd7cde4bae52b42601dab100db708a44a076f
Safari <111dltldnjs@gmail.com>
2023-11-03 20:04:45 +0900

    added: EditProfileView.swift 파일 생성

---
+++ ios-tripbook/View/Mypage/EditProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  EditProfileView.swift
+//  ios-tripbook
+//
+//  Created by 이시원 on 2023/11/03.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditProfileView()
+    }
+}

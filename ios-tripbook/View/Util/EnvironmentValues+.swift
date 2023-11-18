cc58001925550626e21099232b0b714d6f24958f
Safari <111dltldnjs@gmail.com>
2023-11-18 11:21:49 +0900

    feat: rootPresentationMode 구현

---
+++ ios-tripbook/View/Util/EnvironmentValues+.swift
@@ -0,0 +1,28 @@
+//
+//  EnvironmentValues+.swift
+//  ios-tripbook
+//
+//  Created by 이시원 on 11/18/23.
+//
+
+import SwiftUI
+
+struct RootPresentationModeKey: EnvironmentKey {
+    static let defaultValue: Binding<RootPresentationMode> = .constant(RootPresentationMode())
+}
+
+extension EnvironmentValues {
+    var rootPresentationMode: Binding<RootPresentationMode> {
+        get { return self[RootPresentationModeKey.self] }
+        set { self[RootPresentationModeKey.self] = newValue }
+    }
+}
+
+typealias RootPresentationMode = Bool
+
+extension RootPresentationMode {
+    
+    public mutating func dismiss() {
+        self = false
+    }
+}

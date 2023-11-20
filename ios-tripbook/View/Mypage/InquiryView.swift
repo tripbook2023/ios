2f77cb4caf75a191fdafb06790121e693534fb10
Safari <111dltldnjs@gmail.com>
2023-11-20 21:34:14 +0900

    added: add InquiryView.swift

---
+++ ios-tripbook/View/Mypage/InquiryView.swift
@@ -0,0 +1,18 @@
+//
+//  InquiryView.swift
+//  ios-tripbook
+//
+//  Created by 이시원 on 11/20/23.
+//
+
+import SwiftUI
+
+struct InquiryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InquiryView()
+}

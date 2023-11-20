3f980475d7a35f5e19a78969bc134f84fc7b6726
Safari <111dltldnjs@gmail.com>
2023-11-20 22:12:39 +0900

    feat: MypageViewModel 구현

--- ios-tripbook/ViewModel/Mypage/MypageViewModel.swift
+++ ios-tripbook/ViewModel/Mypage/MypageViewModel.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import Combine
+
+@MainActor
+final class MypageViewModel: ObservableObject {
+    private var dataStorage: DataStorage
+    private var tokenStorage: TokenStorage
+    private var anyCancellable = Set<AnyCancellable>()
+    
+    @Published var isPresentInquiryView = false
+    @Published var userInfo: MyProfile?
+    @Published var isShowPopup: Bool = false
+    
+    init(
+        dataStorage: DataStorage = .shared,
+        tokenStorage: TokenStorage = .shared
+    ) {
+        self.dataStorage = dataStorage
+        self.tokenStorage = tokenStorage
+        dataStorage.$user
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] in
+            self?.userInfo = $0
+        }.store(in: &self.anyCancellable)
+    }
+    
+    func deleteToken() {
+        tokenStorage.deleteTokens()
+    }
+}

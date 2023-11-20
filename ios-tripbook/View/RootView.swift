0badd800d00d751d2b0165f08fa1fde7fe4aa2a8
Safari <111dltldnjs@gmail.com>
2023-11-20 22:18:33 +0900

    feat: logoutmessage ui 구현

--- ios-tripbook/View/RootView.swift
+++ ios-tripbook/View/RootView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Combine
 
 /**
  View: 초기 진입 화면
@@ -19,61 +20,100 @@ import SwiftUI
  - MypageView
  */
 struct RootView: View {
-    @ObservedObject var viewModel = RootViewModel()
+    @StateObject var viewModel = RootViewModel()
     @Binding private var isPresented: Bool
     @Environment(\.rootPresentationMode) private var rootPresentationMode: Binding<RootPresentationMode>
+    @State private var anyCancellable = Set<AnyCancellable>()
+    
+    private var deviceWidth: CGFloat {
+        return UIScreen.main.bounds.width
+    }
     
     init( isPresented: Binding<Bool> = .constant(true)) {
         self._isPresented = isPresented
     }
     
     var body: some View {
-        TabView(selection: self.$viewModel.selectedTab) {
-            TravelNewsView()
-                .tabItem {
-                    if viewModel.selectedTab == .home {
-                        TBIcon.navigation.home.active
-                    } else {
-                        TBIcon.navigation.home.normal
+        ZStack {
+            TabView(selection: self.$viewModel.selectedTab) {
+                TravelNewsView()
+                    .tabItem {
+                        if viewModel.selectedTab == .home {
+                            TBIcon.navigation.home.active
+                        } else {
+                            TBIcon.navigation.home.normal
+                        }
+                        
+                        Text("홈")
                     }
-                    
-                    Text("홈")
-                }
-                .tag(RootViewModel.TabType.home)
-            
-            RegisterTravelReportView()
-                .tabItem {
-                    if viewModel.selectedTab == .registerTravelReport {
-                        TBIcon.navigation.plus.active
-                    } else {
-                        TBIcon.navigation.plus.normal
-                    }
-                    
-                    Text("등록")
-                }
-                .tag(RootViewModel.TabType.registerTravelReport)
-            
-            MypageView(logoutAction: {
-                withAnimation(Animation.spring().speed(1)) {
-                    self.rootPresentationMode.wrappedValue.dismiss()
-                    self.isPresented = false
-                }
-            })
-            .tabItem {
-                if viewModel.selectedTab == .profile {
-                    TBIcon.navigation.mypage.active
-                } else {
-                    TBIcon.navigation.mypage.normal
-                }
+                    .tag(RootViewModel.TabType.home)
                 
-                Text("내 정보")
+                RegisterTravelReportView()
+                    .tabItem {
+                        if viewModel.selectedTab == .registerTravelReport {
+                            TBIcon.navigation.plus.active
+                        } else {
+                            TBIcon.navigation.plus.normal
+                        }
+                        
+                        Text("등록")
+                    }
+                    .tag(RootViewModel.TabType.registerTravelReport)
+                
+                MypageView(logoutAction: {
+                    viewModel.isShowLogoutMessage = true
+                })
+                .tabItem {
+                    if viewModel.selectedTab == .profile {
+                        TBIcon.navigation.mypage.active
+                    } else {
+                        TBIcon.navigation.mypage.normal
+                    }
+                    
+                    Text("내 정보")
+                }
+                .tag(RootViewModel.TabType.profile)
+            }.onAppear() {
+                UITabBar.appearance().barTintColor = .white
             }
-            .tag(RootViewModel.TabType.profile)
-        }.onAppear() {
-            UITabBar.appearance().barTintColor = .white
+            .accentColor(TBColor.primary._50)
+            .navigationBarHidden(true)
+            
+            VStack(spacing: 6) {
+                Spacer()
+                Image("Logout")
+                    .resizable()
+                    .frame(width: 240, height: 200)
+                Text("로그아웃 되었습니다.")
+                    .font(TBFont.body_4)
+                    .foregroundStyle(TBColor.grayscale._90)
+                Text("다음에 또 만나요!")
+                    .font(TBFont.caption_1)
+                    .foregroundStyle(TBColor.grayscale._60)
+                Spacer()
+            }
+            .frame(width: deviceWidth)
+            .background(.white)
+            .opacity(viewModel.isShowLogoutMessage ? 1 : 0)
+        }.onAppear {
+            bind()
         }
-        .accentColor(TBColor.primary._50)
-        .navigationBarHidden(true)
+    }
+}
+
+extension RootView {
+    private func bind() {
+        viewModel.$isShowLogoutMessage
+            .filter { $0 }
+            .sink { _ in
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                    withAnimation(Animation.spring().speed(1)) {
+                        self.rootPresentationMode.wrappedValue.dismiss()
+                        self.isPresented = false
+                    }
+                }
+            }
+            .store(in: &anyCancellable)
     }
 }
 

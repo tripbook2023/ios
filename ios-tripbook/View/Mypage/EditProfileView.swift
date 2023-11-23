fe2d69f8163206bdb39ba27f34aea8dddd406042
Safari <111dltldnjs@gmail.com>
2023-11-23 19:25:14 +0900

    teat: editProfile 기능 구현

--- ios-tripbook/View/Mypage/EditProfileView.swift
+++ ios-tripbook/View/Mypage/EditProfileView.swift
@@ -6,65 +6,137 @@
 //
 
 import SwiftUI
+import Combine
+
+import TBImagePicker
 
 struct EditProfileView: View {
     @Environment(\.dismiss) private var dismiss
-    @FocusState private var isFocus: Bool
+    @StateObject private var viewModel = EditProfileViewModel()
+    @State private var anyCancellable = Set<AnyCancellable>()
+    
+    private var deviceHeight: CGFloat {
+        return UIScreen.main.bounds.height
+    }
+    
     var body: some View {
-        VStack(spacing: 40) {
-            TBAppBar(title: "프로필 수정") {
-                dismiss()
-            }
-            
-            
-            ZStack(alignment: .bottom) {
-                Image("DefaultProfileImage")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 80, height: 80)
-                    .clipShape(Circle())
-                    .overlay(
-                        Circle()
-                            .stroke(TBColor.grayscale._10, lineWidth: 0.57)
-                    )
-                HStack {
-                    Spacer()
-                    Button {
-                        
-                    } label: {
-                        TBIcon.cameraFull.iconSize(size: .small)
+        ZStack {
+            VStack(spacing: 40) {
+                TBAppBar(title: "프로필 수정") {
+                    dismiss()
+                }
+                ZStack(alignment: .bottom) {
+                    if let imagedata = viewModel.newProfileImageData {
+                        Image(uiImage: UIImage(data: imagedata)!)
+                            .resizable()
                             .scaledToFill()
-                            .frame(width: 24, height: 24)
-                            .background(.white)
+                            .frame(width: 80, height: 80)
                             .clipShape(Circle())
                             .overlay(
                                 Circle()
-                                    .stroke(TBColor.grayscale._10, lineWidth: 0.5)
-                                    
+                                    .stroke(TBColor.grayscale._10, lineWidth: 0.57)
+                            )
+                    } else {
+                        Image("DefaultProfileImage")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 80, height: 80)
+                            .clipShape(Circle())
+                            .overlay(
+                                Circle()
+                                    .stroke(TBColor.grayscale._10, lineWidth: 0.57)
                             )
                     }
+                    HStack {
+                        Spacer()
+                        Button {
+                            viewModel.isShowOptionView = true
+                        } label: {
+                            TBIcon.cameraFull.iconSize(size: .small)
+                                .scaledToFill()
+                                .frame(width: 24, height: 24)
+                                .background(.white)
+                                .clipShape(Circle())
+                                .overlay(
+                                    Circle()
+                                        .stroke(TBColor.grayscale._10, lineWidth: 0.5)
+                                )
+                        }
+                    }
+                    .frame(width: 80)
                 }
-                .frame(width: 80)
+                .padding(.top, 18)
+                TBTextField(
+                    title: "10자 이내 한글, 영문, 숫자 입력",
+                    text: $viewModel.newName,
+                    isValid: Binding(
+                        get: { self.viewModel.warningMessage == nil },
+                        set: { _ in }
+                    ),
+                    warningMessage: $viewModel.warningMessage,
+                    onSubmitEvent: {
+                        self.viewModel.checkNicknameDuplication()
+                    }
+                )
+                Spacer()
+                TBPrimaryButton(
+                    title: "확인",
+                    isEnabled: Binding(
+                        get: { self.viewModel.warningMessage == nil },
+                        set: { _ in }
+                    ),
+                    onClickedEvent: {
+                        self.viewModel.updateProfile()
+                    }
+                )
+                .padding(.vertical, 18)
             }
-            .padding(.top, 18)
-            TBTextField(
-                title: "10자 이내 한글, 영문, 숫자 입력",
-                text: .constant("safari"),
-                isValid: .constant(false),
-                warningMessage: .constant("xxx"),
-                onSubmitEvent: {
-                    
+            .padding(.horizontal, 20)
+            ProfileImageSelectOptionView(delgate: viewModel)
+                .opacity(viewModel.isShowOptionView ? 1 : 0)
+        }
+        .fullScreenCover(isPresented: self.$viewModel.isShowImagePicker) {
+            TBImagePickerView(
+                .single,
+                onFinish: { assetManagers in
+                    assetManagers.first?.request(
+                        size: .init(width: 140, height: 140),
+                        completion: { image, _ in
+                            viewModel.newProfileImageData = image?.jpegData(compressionQuality: 0.5)
+                        }
+                    )
                 }
             )
-            .focused($isFocus)
-            
-            
-            Spacer()
-            TBPrimaryButton(title: "확인", isEnabled: .constant(true))
-                .padding(.vertical, 18)
         }
-        .padding(.horizontal, 20)
+        .sheet(isPresented: self.$viewModel.isShowCameraView) {
+            ProfileImageCameraView(
+                isPresented: $viewModel.isShowCameraView
+            ) { image in
+                self.viewModel.newProfileImageData = image.jpegData(compressionQuality: 0.5)
+            }
+            .navigationBarHidden(true)
+        }
+        .onAppear {
+            bind()
+        }
         .navigationBarHidden(true)
+    }
+}
+
+extension EditProfileView {
+    private func bind() {
+        viewModel.$isDismiss
+            .filter { $0 }
+            .sink { _ in
+                self.dismiss()
+            }
+            .store(in: &anyCancellable)
+        
+        viewModel.$newName
+            .sink { _ in
+                self.viewModel.checkValidationNickname()
+            }
+            .store(in: &anyCancellable)
     }
 }
 

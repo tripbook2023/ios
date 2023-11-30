e54ad888ccb056cd8f97c403bdadfe294264ca89
Safari <111dltldnjs@gmail.com>
2023-11-30 15:41:55 +0900

    feat: Author

---
+++ ios-tripbook/Model/Author.swift
@@ -0,0 +1,14 @@
+//
+//  Author.swift
+//  ios-tripbook
+//
+//  Created by 이시원 on 11/30/23.
+//
+
+import Foundation
+
+struct Author {
+    let name: String
+    let profileUrl: String
+    let role: String
+}

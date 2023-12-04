5a812415cbf6296917f0b1fdec53202ed1da64b0
Safari <111dltldnjs@gmail.com>
2023-12-04 14:44:09 +0900

    feat: search api 연동

--- ios-tripbook/ViewModel/TravelNews/TravelNewsViewModel.swift
+++ ios-tripbook/ViewModel/TravelNews/TravelNewsViewModel.swift
@@ -6,17 +6,25 @@
 //
 
 import Foundation
-import SwiftUI
 
 /// 여행소식 화면 View Model
 class TravelNewsViewModel: ObservableObject {
-    let dataStorage = DataStorage.shared
+    private let apiManager: APIManagerable
+    private let tokenStorage: TokenStorage
+    
+    init(
+        apiManager: APIManagerable = TBAPIManager(),
+        tokenStorage: TokenStorage = .shared
+    ) {
+        self.apiManager = apiManager
+        self.tokenStorage = tokenStorage
+    }
     
     /// 에디터/관리자 본인 여행소식 게시물 List
-    @Published var myTravelNewsList: [MyTravelNewsModel] = []
+    @Published var myTravelNewsList: [TravelNewsModel] = []
     /// 여행소식 게시물 LIst
     @Published var travelNewsList: [TravelNewsModel] = []
-    
+    @Published var selectTravelNewsItem: TravelNewsModel?
     @Published var isShowEditorView: Bool = false
     @Published var currentSort: Sort = .createdDesc    
     @Published var isSortPopup = false
@@ -24,25 +32,40 @@ class TravelNewsViewModel: ObservableObject {
     /// 현재 여행소식 게시물 List Page Number
     var currentPage = 0
     
-    init() {
-        self.myTravelNewsList = self.fetchMyTravelNewsList()
-        self.travelNewsList = self.fetchTravelNewsList()
-    }
-    
     /// Load 에디터/관리자 본인 여행소식 게시물 LIst
-    func fetchMyTravelNewsList() -> [MyTravelNewsModel] {
+    func fetchMyTravelNewsList() {
         // TODO: API Service 연동
-        return [
-            SampleMyTravelNewsModel(.done),
-            SampleMyTravelNewsModel(.done),
-            SampleMyTravelNewsModel(.done),
-            SampleMyTravelNewsModel(.waiting)
+        myTravelNewsList = [
+            TravelNewsModel.dummy,
+            TravelNewsModel.dummy,
+            TravelNewsModel.dummy,
+            TravelNewsModel.dummy
         ]
     }
     
     /// Load 여행소식 게시물 List
-    func fetchTravelNewsList() -> [TravelNewsModel] {
+    func fetchTravelNewsList() {
         // TODO: API Service 연동
-        return Array(repeating: SampleTravelNewsModel(), count: 5)
+        Task {
+            do {
+                let api = TBTravelNewsAPI.search(
+                    accessToken: tokenStorage.accessToken ?? "",
+                    word: "",
+                    page: 0,
+                    size: 10,
+                    sort: currentSort
+                )
+                let items = try await apiManager.request(
+                    api,
+                    type: SearchResponse.self
+                ).toDomain
+                await MainActor.run {
+                    travelNewsList = items
+                }
+            } catch {
+                
+            }
+            
+        }
     }
 }

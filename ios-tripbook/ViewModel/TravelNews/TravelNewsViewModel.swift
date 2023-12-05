e54636d6f2d5071f159eb411aec3af962d2a7a94
Safari <111dltldnjs@gmail.com>
2023-12-05 16:22:46 +0900

    feat: 검색 기능을 위한 변수 및 메서드 추가 구현

--- ios-tripbook/ViewModel/TravelNews/TravelNewsViewModel.swift
+++ ios-tripbook/ViewModel/TravelNews/TravelNewsViewModel.swift
@@ -11,13 +11,16 @@ import Foundation
 class TravelNewsViewModel: ObservableObject {
     private let apiManager: APIManagerable
     private let tokenStorage: TokenStorage
+    private let searchKeywordStorage: Storageable
     
     init(
         apiManager: APIManagerable = TBAPIManager(),
-        tokenStorage: TokenStorage = .shared
+        tokenStorage: TokenStorage = .shared,
+        searchKeywordStorage: Storageable = SearchKeywordStorage()
     ) {
         self.apiManager = apiManager
         self.tokenStorage = tokenStorage
+        self.searchKeywordStorage = searchKeywordStorage
     }
     
     /// 에디터/관리자 본인 여행소식 게시물 List
@@ -25,6 +28,9 @@ class TravelNewsViewModel: ObservableObject {
     /// 여행소식 게시물 LIst
     @Published var travelNewsList: [TravelNewsModel] = []
     @Published var selectTravelNewsItem: TravelNewsModel?
+    @Published var isSearch = false
+    @Published var searchKeyword = ""
+    @Published var keywordList: [String] = []
     @Published var isShowEditorView: Bool = false
     @Published var currentSort: Sort = .createdDesc    
     @Published var isSortPopup = false
@@ -67,5 +73,32 @@ class TravelNewsViewModel: ObservableObject {
             }
             
         }
+    }
+    
+    func readSearchKeywords() {
+        keywordList = searchKeywordStorage.read().reversed()
+    }
+    
+    func addSearchKeyword() {
+        guard !searchKeyword.isEmpty else { return }
+        if let index = self.searchKeywordStorage
+            .read()
+            .firstIndex(where: { $0 == searchKeyword })
+        {
+            try? self.searchKeywordStorage.delete(index: index)
+        }
+        try? self.searchKeywordStorage.save(searchKeyword)
+        keywordList = searchKeywordStorage.read().reversed()
+    }
+    
+    func deleteSearchKeyword(index: Int) {
+        let count = keywordList.count
+        try? searchKeywordStorage.delete(index: count - index - 1)
+        keywordList = searchKeywordStorage.read().reversed()
+    }
+    
+    func deleteAllSearchKeyword() {
+        try? searchKeywordStorage.deleteAll()
+        keywordList = []
     }
 }

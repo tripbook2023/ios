bf815a4cbfcbdd65d368c43810459dad1bace817
Safari <111dltldnjs@gmail.com>
2023-12-29 20:26:25 +0900

    feat: RegisterTravelNewsViewModel

---
+++ ios-tripbook/ViewModel/TravelNews/RegisterTravelNewsViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  RegisterTravelNewsViewModel.swift
+//  ios-tripbook
+//
+//  Created by 이시원 on 12/29/23.
+//
+
+import Foundation
+
+final class RegisterTravelNewsViewModel: ObservableObject {
+    private var apiManager: APIManagerable
+    
+    @Published var tempItems: [TravelNewsModel] = []
+    
+    init(apiManager: APIManagerable = TBAPIManager()) {
+        self.apiManager = apiManager
+    }
+    
+    func fatchTempList() {
+        Task {
+            do {
+                let api = TBMemberAPI.selectTemp()
+                let result = try await apiManager.request(api, type: [ContentResponse].self).map { $0.toDomain }
+                await MainActor.run {
+                    tempItems = result
+                }
+            } catch {
+                // 에러처리
+            }
+           
+        }
+    }
+    
+    
+}

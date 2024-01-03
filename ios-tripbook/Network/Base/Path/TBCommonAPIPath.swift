e9f6e31742eaf6637ceb2e29dded2e7fa06d0244
Safari <91936941+Siwon-L@users.noreply.github.com>
2024-01-03 22:07:25 +0900

    Feature/save my travel api (#72)
    * feat: TBCommonAPIPath 생성
    
    * feat: TBCommonAPI 생성
    
    ---------
    
    Co-authored-by: Red <seryprk@gmail.com>

---
+++ ios-tripbook/Network/Base/Path/TBCommonAPIPath.swift
@@ -0,0 +1,12 @@
+//
+//  TBCommonAPIPath.swift
+//  ios-tripbook
+//
+//  Created by RED on 2023/12/27.
+//
+
+import Foundation
+
+struct TBCommonAPIPath {
+    let imageUpload = "/common/image/upload"
+}
